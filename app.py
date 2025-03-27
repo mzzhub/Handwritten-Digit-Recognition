bafc33b1e671c5a565cf15ffc8dd3a1c01805e2b
mzhub <mishabhadhi@gmail.com>
2025-03-27 17:50:33 +0000

    push

---
+++ app.py
@@ -0,0 +1,12 @@
+import pygame#, sys
+# from pygame.locals import *
+# import numpy as np
+# from keras.models import load_model
+# import cv2
+
+
+# Initialize pygame
+pygame.init()
+WINDOWSIZEX = 640
+WINDOWSIZEY = 480
+pygame.display.set_mode((WINDOWSIZEX, WINDOWSIZEY))
